a933978f04d95a085f9f5b3c94a42b30e818c7ec
piseth sattya <pisethsattya33@gmail.com>
2024-12-21 15:39:36 +0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,13 @@ import './App.css';
 
 function App() {
   const audioRef = useRef(null);
-
-  useEffect(() => {
-    audioRef.current = new Audio("/music/Shane Filan - Beautiful In White (Official Video).mp3");
-    audioRef.current.loop = true; // Set the audio to loop
-    audioRef.current.play() // Attempt to autoplay the audio
-      .catch(error => {
-        console.error('Auto-play failed', error);
-        // Handle the failure of auto-play here if needed, e.g., show a play button
-      });
-
-    return () => {
-      if (audioRef.current) {
-        audioRef.current.pause();
-        audioRef.current.src = ""; // Release the audio resource
-      }
-    };
-  }, []);
-
+  useEffect(()=>{
+    audioRef.current.play();
+  },[])
   return (
     <div className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'>
+      <audio ref={audioRef} src="/music/Shane Filan - Beautiful In White (Official Video).mp3" loop autoPlay>
+      </audio>
       <div className='md:flex'>
         <div className='md:shrink-0 w-full'>
           <img src="/images/Grey modern wedding invitation (1).png" alt="Wedding invitation" loading="lazy" />
