8be474a57584befc3d24ee2c9c45a2bac2269250
piseth sattya <pisethsattya33@gmail.com>
2024-12-21 15:47:42 +0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import './App.css';
 
 function App() {
   const audioRef = useRef(null);
-  useEffect(()=>{
-    audioRef.current.play();
-  },[])
+
+  useEffect(() => {
+    const playAudio = () => {
+      audioRef.current.play().catch(error => console.error("Error playing audio:", error));
+    };
+
+    document.addEventListener('click', playAudio, { once: true }); // Play audio on first click anywhere
+    return () => document.removeEventListener('click', playAudio);
+  }, []);
+
   return (
     <div className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'>
-      <audio ref={audioRef} src="/music/Shane Filan - Beautiful In White (Official Video).mp3" loop autoPlay>
+      <audio ref={audioRef} src="/music/Shane Filan - Beautiful In White (Official Video).mp3" loop>
       </audio>
       <div className='md:flex'>
         <div className='md:shrink-0 w-full'>
